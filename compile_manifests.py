ec2a6a26f4d62312e68ddb403fba33a2abaaf81d
Cliff Wulfman <cwulfman@princeton.edu>
2025-04-11 10:36:31 -0400

    what I think is working compiler

--- compile_manifests.py
+++ compile_manifests.py
@@ -2,11 +2,20 @@ from pathlib import Path
 from compiler import Db, Compiler
 
 rdf_dir = Path("/Users/wulfmanc/repos/gh/perseus-aa/rdf")
+out_dir = Path("/tmp/output")
+manifest_out = out_dir / Path("manifests")
+webpage_out = out_dir / Path("webpages")
 
-db:Db = Db()
+
+def main():
+    db:Db = Db()
+    db.load_all(rdf_dir)
+    db.load_all(rdf_dir / Path("object_image_graphs"))
+
+    c = Compiler(db)
+    c.compile_manifests(manifest_out)
+    c.compile_web_pages(webpage_out)
 
 
-db.load_all(rdf_dir)
-db.load_all(rdf_dir / Path("object_image_graphs"))
-
-c = Compiler(db)
+if __name__ == "__main__":
+    main()
